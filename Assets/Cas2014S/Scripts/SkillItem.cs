07a46d8732cc585841b143d13be9165664cb903d
himapo <datalocal@gmail.com>
2014-03-15 21:30:37 +0900

    スキルを拾えるようにした

--- Assets/Cas2014S/Scripts/SkillItem.cs
+++ Assets/Cas2014S/Scripts/SkillItem.cs
@@ -1,29 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GunItem : MyBehaviour {
-
-	public PlayerGun gun;
-
+public class SkillItem : MyBehaviour {
+	
+	public Skill skill;
+	
 	bool isShowPick;
-
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		
 	}
-
+	
 	void OnGUI()
 	{
 		if(!isShowPick)
 		{
 			return;
 		}
-
+		
 		var topleft = Camera.main.ViewportToScreenPoint(new Vector3(0.35f, 0.2f, 0));
 		
 		var size = Camera.main.ViewportToScreenPoint(new Vector3(0.3f, 0.4f));
@@ -31,85 +31,85 @@ public class GunItem : MyBehaviour {
 		var area = new Rect(
 			topleft.x, topleft.y,
 			size.x, size.y);
-
+		
 		GUILayout.BeginArea(area, "", "box");
 		{
 			GUILayout.BeginHorizontal();
 			{
 				//GUILayout.FlexibleSpace();
-			
+				
 				var labelStyle = new GUIStyle(GUI.skin.GetStyle("label"));
 				labelStyle.alignment = TextAnchor.MiddleCenter;
 				labelStyle.fontSize = 30;
-
+				
 				GUILayout.Label(
-					"銃を拾った！",
+					"スキルを拾った！",
 					labelStyle);
-
+				
 				//GUILayout.FlexibleSpace();
 			}	
 			GUILayout.EndHorizontal();
-
+			
 			GUILayout.FlexibleSpace();
-
+			
 			GUILayout.BeginHorizontal("box", GUILayout.MinHeight(100));
 			{
 				GUILayout.BeginVertical();
 				{
-		
+					
 					GUILayout.FlexibleSpace();
-		
+					
 					if(GUILayout.Button("左手に装備"))
 					{
-						GunController.ChangeGun(0, gun.Type);
+						GetGun(0).AddSkill(skill.Type);
 						KillMe();
 					}
 					
 					GUILayout.FlexibleSpace();
-		
+					
 				}
 				GUILayout.EndVertical();
-		
+				
 				GUILayout.FlexibleSpace();
-
+				
 				GUILayout.BeginVertical();
 				{
 					GUILayout.FlexibleSpace();
-				
-					IconDrawer.Instance.DrawGun(gun);
-				
+					
+					IconDrawer.Instance.DrawShopSkill(skill, Vector3.zero, true);
+					
 					GUILayout.FlexibleSpace();
 				}
 				GUILayout.EndVertical();
-
+				
 				GUILayout.FlexibleSpace();
-
+				
 				GUILayout.BeginVertical();
 				{
 					GUILayout.FlexibleSpace();
 					
 					if(GUILayout.Button("右手に装備"))
 					{
-						GunController.ChangeGun(1, gun.Type);
+						GetGun(1).AddSkill(skill.Type);
 						KillMe();
 					}
 					
 					GUILayout.FlexibleSpace();
 				}
 				GUILayout.EndVertical();
-		
+				
 			}
 			GUILayout.EndHorizontal();
-
+			
 			GUILayout.FlexibleSpace();
-
+			
 			GUILayout.BeginHorizontal();
 			{
 				GUILayout.FlexibleSpace();
 				
 				var buttonStyle = new GUIStyle(GUI.skin.GetStyle("button"));
 				buttonStyle.fontSize = 16;
-
+				
 				if(GUILayout.Button("いらない", buttonStyle, GUILayout.MinHeight(50)))
 				{
 					isShowPick = false;
@@ -123,7 +123,7 @@ public class GunItem : MyBehaviour {
 		GUILayout.EndArea();
 		
 	}
-
+	
 	void OnTriggerExit(Collider collider)
 	{
 		if(collider.tag == "Player")
@@ -131,19 +131,19 @@ public class GunItem : MyBehaviour {
 			ButtonHelp.Instance.SetShow("pick", false);
 		}
 	}
-
+	
 	void OnTriggerStay(Collider collider)
 	{
 		if(isShowPick)
 		{
 			return;
 		}
-
+		
 		if(collider.tag != "Player")
 		{
 			return;
 		}
-
+		
 		if(Input.GetButton("Use"))
 		{
 			isShowPick = true;
@@ -151,10 +151,10 @@ public class GunItem : MyBehaviour {
 			ButtonHelp.Instance.SetShow("pick", false);
 			return;
 		}
-
+		
 		ButtonHelp.Instance.SetShow("pick", true);
 	}
-
+	
 	void KillMe()
 	{
 		isShowPick = false;
@@ -162,24 +162,24 @@ public class GunItem : MyBehaviour {
 		ButtonHelp.Instance.SetShow("pick", false);
 		Destroy(gameObject);
 	}
-
+	
 	void OnBeginFloorMove()
 	{
 		ButtonHelp.Instance.SetShow("pick", false);
 		Destroy(gameObject);
 	}
-
+	
 	void OnGotoTitle()
 	{
 		ButtonHelp.Instance.SetShow("pick", false);
 		Destroy(gameObject);
 	}
-
+	
 	void OnPauseMenuOpen()
 	{
 		enabled = false;
 	}
-
+	
 	void OnPauseMenuClose()
 	{
 		enabled = true;
