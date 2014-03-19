5ffb97bf459ea79dc38c411cbfd681d1474d7e13
himapo <datalocal@gmail.com>
2014-03-19 10:04:31 +0900

    出口に触れているときに次のフロアまでの時間を表示するようにした

--- Assets/Cas2014S/Scripts/ExitCollision.cs
+++ Assets/Cas2014S/Scripts/ExitCollision.cs
@@ -20,9 +20,58 @@ public class ExitCollision : MyBehaviour {
 	
 	}
 
+	void OnGUI()
+	{
+		if(!enter)
+		{
+			return;
+		}
+
+		GUI.color = Color.green;
+
+		var topleft = Camera.main.ViewportToScreenPoint(
+			new Vector3(0.1f, 0.35f, 0.0f));
+		
+		var size = Camera.main.ViewportToScreenPoint(
+			new Vector3(0.8f, 0.1f, 0.0f));
+		
+		var area = new Rect(
+			topleft.x, topleft.y,
+			size.x, size.y);
+
+		GUILayout.BeginArea(area);
+
+		GUILayout.BeginHorizontal();
+		
+		GUILayout.FlexibleSpace();
+
+		var labelStyle = new GUIStyle(GUI.skin.GetStyle("label"));
+		labelStyle.alignment = TextAnchor.MiddleCenter;
+		labelStyle.fontSize = 24;
+
+		GUILayout.Label(
+			string.Format("次のフロアまであと{0:F2}秒", GetStayTime() - elapsed),
+			labelStyle,
+			GUILayout.MinWidth(360));
+
+		GUILayout.FlexibleSpace();
+
+		GUILayout.EndHorizontal();
+
+		GUILayout.EndArea();
+	}
+
+	float GetStayTime()
+	{
+		return stayTime;
+	}
+
 	void OnTriggerEnter(Collider other)
 	{
-		enter = true;
+		if(other.tag == "Player")
+		{
+			enter = true;
+		}
 	}
 
 	void OnTriggerStay(Collider other)
@@ -32,7 +81,7 @@ public class ExitCollision : MyBehaviour {
 			elapsed += Time.deltaTime;
 		}
 
-		if(enter && elapsed >= stayTime)
+		if(enter && elapsed >= GetStayTime())
 		{
 			Debug.Log("Go to Next Floor");
 			GameController.SendMessage("GotoNextFloor");
